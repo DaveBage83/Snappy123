7492ee181f44eac9d1a96dcd41f5f3fe3ebba03e
Kevin Palser <kevin.palser@snappyshopper.co.uk>
2022-01-12 09:57:00 +0000

    BasketItemMissedPromotion added to the BasketItem Model

--- SnappyV2Tests/UI/Basket View/BasketListItemViewModelTests.swift
+++ SnappyV2Tests/UI/Basket View/BasketListItemViewModelTests.swift
@@ -13,7 +13,7 @@ class BasketListItemViewModelTests: XCTestCase {
     func test_init() {
         let storeMenuItemPrice = RetailStoreMenuItemPrice(price: 10, fromPrice: 9, unitMetric: "", unitsInPack: 0, unitVolume: 0, wasPrice: nil)
         let storeMenuItem = RetailStoreMenuItem(id: 123, name: "ItemName", eposCode: nil, outOfStock: false, ageRestriction: 0, description: nil, quickAdd: true, price: storeMenuItemPrice, images: nil, menuItemSizes: nil, menuItemOptions: nil, availableDeals: nil)
-        let basketItem = BasketItem(basketLineId: 321, menuItem: storeMenuItem, totalPrice: 10, totalPriceBeforeDiscounts: 9, price: 9, pricePaid: 9, quantity: 0, size: nil, selectedOptions: nil)
+        let basketItem = BasketItem(basketLineId: 321, menuItem: storeMenuItem, totalPrice: 10, totalPriceBeforeDiscounts: 9, price: 9, pricePaid: 9, quantity: 0, size: nil, selectedOptions: nil, missedPromotions: nil)
         let sut = makeSUT(item: basketItem, changeQuantity: {_, _, _ in})
         
         XCTAssertEqual(sut.item, basketItem)
@@ -23,7 +23,7 @@ class BasketListItemViewModelTests: XCTestCase {
     func test_givenBasketItem_whenOnSubmit_thenQuantityIsResetAndClosureReturns() {
         let storeMenuItemPrice = RetailStoreMenuItemPrice(price: 10, fromPrice: 9, unitMetric: "", unitsInPack: 0, unitVolume: 0, wasPrice: nil)
         let storeMenuItem = RetailStoreMenuItem(id: 123, name: "ItemName", eposCode: nil, outOfStock: false, ageRestriction: 0, description: nil, quickAdd: true, price: storeMenuItemPrice, images: nil, menuItemSizes: nil, menuItemOptions: nil, availableDeals: nil)
-        let basketItem = BasketItem(basketLineId: 321, menuItem: storeMenuItem, totalPrice: 10, totalPriceBeforeDiscounts: 9, price: 9, pricePaid: 9, quantity: 0, size: nil, selectedOptions: nil)
+        let basketItem = BasketItem(basketLineId: 321, menuItem: storeMenuItem, totalPrice: 10, totalPriceBeforeDiscounts: 9, price: 9, pricePaid: 9, quantity: 0, size: nil, selectedOptions: nil, missedPromotions: nil)
         let sut = makeSUT(item: basketItem) { itemId, quantity, basketLineId in
             XCTAssertEqual(itemId, 123)
             XCTAssertEqual(quantity, 2)
@@ -39,7 +39,7 @@ class BasketListItemViewModelTests: XCTestCase {
     func test_whenFilterQuantityToStringNumberIsTriggered_thenOnlyNumberIsStored() {
         let storeMenuItemPrice = RetailStoreMenuItemPrice(price: 10, fromPrice: 9, unitMetric: "", unitsInPack: 0, unitVolume: 0, wasPrice: nil)
         let storeMenuItem = RetailStoreMenuItem(id: 123, name: "ItemName", eposCode: nil, outOfStock: false, ageRestriction: 0, description: nil, quickAdd: true, price: storeMenuItemPrice, images: nil, menuItemSizes: nil, menuItemOptions: nil, availableDeals: nil)
-        let basketItem = BasketItem(basketLineId: 321, menuItem: storeMenuItem, totalPrice: 10, totalPriceBeforeDiscounts: 9, price: 9, pricePaid: 9, quantity: 0, size: nil, selectedOptions: nil)
+        let basketItem = BasketItem(basketLineId: 321, menuItem: storeMenuItem, totalPrice: 10, totalPriceBeforeDiscounts: 9, price: 9, pricePaid: 9, quantity: 0, size: nil, selectedOptions: nil, missedPromotions: nil)
         let sut = makeSUT(item: basketItem, changeQuantity: {_, _, _ in})
         
         sut.filterQuantityToStringNumber(stringValue: "OneTwo12")

433b41ca7782d8234be99082d5e69d361cbfc525
dvbage <david.bage@snappyshopper.co.uk>
2022-01-13 16:45:50 +0000

    Add snapshot tests

---
+++ SnappyV2SnapshotTests/Components/SpecialOfferPillViewTests.swift
@@ -0,0 +1,26 @@
+//
+//  SpecialOfferPillViewTests.swift
+//  SnappyV2SnapshotTests
+//
+//  Created by David Bage on 13/01/2022.
+//
+
+import XCTest
+import SwiftUI
+@testable import SnappyV2
+
+class SpecialOfferPillViewTests: XCTestCase {
+
+    func test_init() {
+        let sut = makeSUT()
+        let iPhone12Snapshot = sut.snapshot(for: .iPhone12(style: .light))
+        let iPad8thGenSnapshot = sut.snapshot(for: .iPad8thGen(style: .light))
+        
+        assert(snapshot: iPhone12Snapshot, sut: sut)
+        assert(snapshot: iPad8thGenSnapshot, sut: sut)
+    }
+    
+    func makeSUT() -> SpecialOfferPill {
+        SpecialOfferPill(offerText: "Test")
+    }
+}

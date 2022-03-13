3dc3ddbce307d2dacf877915b871b54656059e31
Kevin Palser <kevin.palser@snappyshopper.co.uk>
2022-03-13 09:53:37 +0000

    progress on retail service layer tests

--- SnappyV2Tests/Services/RetailStoresServiceTests.swift
+++ SnappyV2Tests/Services/RetailStoresServiceTests.swift
@@ -40,52 +40,18 @@ class RetailStoresServiceTests: XCTestCase {
 }
 
 /*
-func searchRetailStores(search: LoadableSubject<RetailStoresSearch>, postcode: String)
-func searchRetailStores(search: LoadableSubject<RetailStoresSearch>, location: CLLocationCoordinate2D)
-func repeatLastSearch(search: LoadableSubject<RetailStoresSearch>)
-func getStoreDetails(details: LoadableSubject<RetailStoreDetails>, storeId: Int, postcode: String)
+func searchRetailStores(postcode: String)
+func searchRetailStores(location: CLLocationCoordinate2D)
+func repeatLastSearch()
+func getStoreDetails(storeId: Int, postcode: String)
 func getStoreDeliveryTimeSlots(slots: LoadableSubject<RetailStoreTimeSlots>, storeId: Int, startDate: Date, endDate: Date, location: CLLocationCoordinate2D)
 func getStoreCollectionTimeSlots(slots: LoadableSubject<RetailStoreTimeSlots>, storeId: Int, startDate: Date, endDate: Date)
 */
 
-// MARK: - func searchRetailStores(search:postcode:)
+// MARK: - func searchRetailStores(postcode:)
 final class SearchRetailStoresByPostcodeTests: RetailStoresServiceTests {
     
-    func test_successfulSearch() {
-        
-//        let searchResult = RetailStoresSearch.mockedData
-//
-//        // Configuring expected actions on repositories
-//
-//        mockedWebRepo.actions = .init(expected: [
-//            .loadRetailStores(postcode: searchResult.fulfilmentLocation.postcode)
-//        ])
-//        mockedDBRepo.actions = .init(expected: [
-//            .clearSearches,
-//            .store(searchResult: searchResult, forPostode: "DD1 3JA")
-//        ])
-//
-//        // Configuring responses from repositories
-//
-//        mockedWebRepo.loadRetailStoresByPostcodeResponse = .success(searchResult)
-//        mockedDBRepo.clearSearchesResult = .success(true)
-//        mockedDBRepo.storeByLocation = .success(searchResult)
-//        mockedDBRepo.fetchRetailStoresSearchByPostcodeResult = .success(searchResult)
-//
-//        let search = BindingWithPublisher(value: Loadable<RetailStoresSearch>.notRequested)
-//        sut.searchRetailStores(postcode: searchResult.fulfilmentLocation.postcode)
-//        let exp = XCTestExpectation(description: #function)
-//        search.updatesRecorder.sink { updates in
-//            XCTAssertEqual(updates, [
-//                .notRequested,
-//                .isLoading(last: nil, cancelBag: CancelBag()),
-//                .loaded(searchResult)
-//            ], removing: RetailStoresSearch.prefixes)
-//            self.mockedWebRepo.verify()
-//            self.mockedDBRepo.verify()
-//            exp.fulfill()
-//        }.store(in: &subscriptions)
-//        wait(for: [exp], timeout: 2)
+    func test_successfulSearch_setAppSearchResultState() {
         
         let searchResult = RetailStoresSearch.mockedData
         
@@ -103,8 +69,7 @@ final class SearchRetailStoresByPostcodeTests: RetailStoresServiceTests {
 
         mockedWebRepo.loadRetailStoresByPostcodeResponse = .success(searchResult)
         mockedDBRepo.clearSearchesResult = .success(true)
-        mockedDBRepo.storeByLocation = .success(searchResult)
-        mockedDBRepo.fetchRetailStoresSearchByPostcodeResult = .success(searchResult)
+        mockedDBRepo.storeByPostcode = .success(searchResult)
         
         XCTAssertEqual(AppState().userData.searchResult, .notRequested)
         let exp = XCTestExpectation(description: #function)
@@ -122,59 +87,287 @@ final class SearchRetailStoresByPostcodeTests: RetailStoresServiceTests {
     }
     
     
-    func test_filledDB_successfulSearch() {
+    func test_unsuccessfulSearch_whenNetworkErrorAndNoPreviousResult_nilAppSearchResultState() {
+        
+        let networkError = NSError(domain: NSURLErrorDomain, code: -1009, userInfo: [:])
+        let searchResult = RetailStoresSearch.mockedData
+        
+        // Configuring expected actions on repositories
+
+        mockedWebRepo.actions = .init(expected: [
+            .loadRetailStores(postcode: searchResult.fulfilmentLocation.postcode)
+        ])
+        mockedDBRepo.actions = .init(expected: [
+            .clearSearches
+        ])
+
+        // Configuring responses from repositories
+
+        mockedWebRepo.loadRetailStoresByPostcodeResponse = .failure(networkError)
+        mockedDBRepo.clearSearchesResult = .success(true)
+        
+        XCTAssertEqual(AppState().userData.searchResult, .notRequested)
+        let exp = XCTestExpectation(description: #function)
+        sut.searchRetailStores(postcode: searchResult.fulfilmentLocation.postcode)
+        delay {
+            XCTAssertEqual(
+                self.sut.appState.value.userData.searchResult.value,
+                nil
+            )
+            self.mockedWebRepo.verify()
+            self.mockedDBRepo.verify()
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 0.5)
         
     }
     
-    func test_filledDB_failedSearch() {
+    func test_unsuccessfulSearch_whenNetworkErrorAndPreviousResult_nilAppSearchResultState() {
+        
+        let networkError = NSError(domain: NSURLErrorDomain, code: -1009, userInfo: [:])
+        let searchResult = RetailStoresSearch.mockedData
+        
+        // Configuring app prexisting states
+        appState.value.userData.searchResult = .loaded(searchResult)
+        
+        // Configuring expected actions on repositories
+
+        mockedWebRepo.actions = .init(expected: [
+            .loadRetailStores(postcode: searchResult.fulfilmentLocation.postcode)
+        ])
+        mockedDBRepo.actions = .init(expected: [
+            .clearSearches
+        ])
+
+        // Configuring responses from repositories
+
+        mockedWebRepo.loadRetailStoresByPostcodeResponse = .failure(networkError)
+        mockedDBRepo.clearSearchesResult = .success(true)
+        
+        XCTAssertEqual(AppState().userData.searchResult, .notRequested)
+        let exp = XCTestExpectation(description: #function)
+        sut.searchRetailStores(postcode: searchResult.fulfilmentLocation.postcode)
+        delay {
+            XCTAssertEqual(
+                self.sut.appState.value.userData.searchResult.value,
+                nil
+            )
+            self.mockedWebRepo.verify()
+            self.mockedDBRepo.verify()
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 0.5)
         
     }
     
-    func test_emptyDB_failedRequest() {
+    func test_unsuccessfulSearch_whenUnableToSaveResult_nilAppSearchResultState() {
+        
+        let dbError = NSError(domain: "CoreData", code: -1009, userInfo: [:])
+        let searchResult = RetailStoresSearch.mockedData
+        
+        // Configuring expected actions on repositories
+
+        mockedWebRepo.actions = .init(expected: [
+            .loadRetailStores(postcode: searchResult.fulfilmentLocation.postcode)
+        ])
+        mockedDBRepo.actions = .init(expected: [
+            .clearSearches,
+            .store(searchResult: searchResult, forPostode: "DD1 3JA")
+        ])
+
+        // Configuring responses from repositories
+
+        mockedWebRepo.loadRetailStoresByPostcodeResponse = .success(searchResult)
+        mockedDBRepo.clearSearchesResult = .success(true)
+        mockedDBRepo.storeByPostcode = .failure(dbError)
+        
+        XCTAssertEqual(AppState().userData.searchResult, .notRequested)
+        let exp = XCTestExpectation(description: #function)
+        sut.searchRetailStores(postcode: searchResult.fulfilmentLocation.postcode)
+        delay {
+            XCTAssertEqual(
+                self.sut.appState.value.userData.searchResult.value,
+                nil
+            )
+            self.mockedWebRepo.verify()
+            self.mockedDBRepo.verify()
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 0.5)
         
     }
-    
-    func test_emptyDB_successfulRequest_successfulStoring() {
-        
-    }
-    
-    func test_emptyDB_successfulRequest_failedStoring() {
-        
-    }
-    
-    //func searchRetailStores(search: LoadableSubject<RetailStoresSearch>, location: CLLocationCoordinate2D)
     
 }
 
 // MARK: - func searchRetailStores(search: location:)
 final class SearchRetailStoresByLocationTests: RetailStoresServiceTests {
     
-    func test_filledDB_successfulSearch() {
+    func test_successfulSearch_setAppSearchResultState() {
+        
+        let searchResult = RetailStoresSearch.mockedData
+        
+        // Configuring expected actions on repositories
 
+        mockedWebRepo.actions = .init(expected: [
+            .loadRetailStores(location: searchResult.fulfilmentLocation.location)
+        ])
+        mockedDBRepo.actions = .init(expected: [
+            .clearSearches,
+            .store(searchResult: searchResult, location: searchResult.fulfilmentLocation.location)
+        ])
+
+        // Configuring responses from repositories
+
+        mockedWebRepo.loadRetailStoresByLocationResponse = .success(searchResult)
+        mockedDBRepo.clearSearchesResult = .success(true)
+        mockedDBRepo.storeByLocation = .success(searchResult)
+        
+        XCTAssertEqual(AppState().userData.searchResult, .notRequested)
+        let exp = XCTestExpectation(description: #function)
+        sut.searchRetailStores(location: searchResult.fulfilmentLocation.location)
+        delay {
+            XCTAssertEqual(
+                self.sut.appState.value.userData.searchResult.value,
+                searchResult
+            )
+            self.mockedWebRepo.verify()
+            self.mockedDBRepo.verify()
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 0.5)
     }
     
-    func test_filledDB_failedSearch() {
+    
+    func test_unsuccessfulSearch_whenNetworkErrorAndNoPreviousResult_nilAppSearchResultState() {
+        
+        let networkError = NSError(domain: NSURLErrorDomain, code: -1009, userInfo: [:])
+        let searchResult = RetailStoresSearch.mockedData
+        
+        // Configuring expected actions on repositories
+
+        mockedWebRepo.actions = .init(expected: [
+            .loadRetailStores(location: searchResult.fulfilmentLocation.location)
+        ])
+        mockedDBRepo.actions = .init(expected: [
+            .clearSearches
+        ])
+
+        // Configuring responses from repositories
+
+        mockedWebRepo.loadRetailStoresByLocationResponse = .failure(networkError)
+        mockedDBRepo.clearSearchesResult = .success(true)
+        
+        XCTAssertEqual(AppState().userData.searchResult, .notRequested)
+        let exp = XCTestExpectation(description: #function)
+        sut.searchRetailStores(location: searchResult.fulfilmentLocation.location)
+        delay {
+            XCTAssertEqual(
+                self.sut.appState.value.userData.searchResult.value,
+                nil
+            )
+            self.mockedWebRepo.verify()
+            self.mockedDBRepo.verify()
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 0.5)
         
     }
     
-    func test_emptyDB_failedRequest() {
+    func test_unsuccessfulSearch_whenNetworkErrorAndPreviousResult_nilAppSearchResultState() {
+        
+        let networkError = NSError(domain: NSURLErrorDomain, code: -1009, userInfo: [:])
+        let searchResult = RetailStoresSearch.mockedData
+        
+        // Configuring app prexisting states
+        appState.value.userData.searchResult = .loaded(searchResult)
+        
+        // Configuring expected actions on repositories
+
+        mockedWebRepo.actions = .init(expected: [
+            .loadRetailStores(location: searchResult.fulfilmentLocation.location)
+        ])
+        mockedDBRepo.actions = .init(expected: [
+            .clearSearches
+        ])
+
+        // Configuring responses from repositories
+
+        mockedWebRepo.loadRetailStoresByLocationResponse = .failure(networkError)
+        mockedDBRepo.clearSearchesResult = .success(true)
+        
+        XCTAssertEqual(AppState().userData.searchResult, .notRequested)
+        let exp = XCTestExpectation(description: #function)
+        sut.searchRetailStores(location: searchResult.fulfilmentLocation.location)
+        delay {
+            XCTAssertEqual(
+                self.sut.appState.value.userData.searchResult.value,
+                nil
+            )
+            self.mockedWebRepo.verify()
+            self.mockedDBRepo.verify()
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 0.5)
         
     }
     
-    func test_emptyDB_successfulRequest_successfulStoring() {
+    func test_unsuccessfulSearch_whenUnableToSaveResult_nilAppSearchResultState() {
         
-    }
-    
-    func test_emptyDB_successfulRequest_failedStoring() {
+        let dbError = NSError(domain: "CoreData", code: -1009, userInfo: [:])
+        let searchResult = RetailStoresSearch.mockedData
+        
+        // Configuring expected actions on repositories
+
+        mockedWebRepo.actions = .init(expected: [
+            .loadRetailStores(location: searchResult.fulfilmentLocation.location)
+        ])
+        mockedDBRepo.actions = .init(expected: [
+            .clearSearches,
+            .store(searchResult: searchResult, location: searchResult.fulfilmentLocation.location)
+        ])
+
+        // Configuring responses from repositories
+
+        mockedWebRepo.loadRetailStoresByLocationResponse = .success(searchResult)
+        mockedDBRepo.clearSearchesResult = .success(true)
+        mockedDBRepo.storeByPostcode = .failure(dbError)
+        
+        XCTAssertEqual(AppState().userData.searchResult, .notRequested)
+        let exp = XCTestExpectation(description: #function)
+        sut.searchRetailStores(location: searchResult.fulfilmentLocation.location)
+        delay {
+            XCTAssertEqual(
+                self.sut.appState.value.userData.searchResult.value,
+                nil
+            )
+            self.mockedWebRepo.verify()
+            self.mockedDBRepo.verify()
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 0.5)
         
     }
 
 }
 
-// MARK: - func clearLastSearch()
-final class ClearLastRetailStoresSearchTests: RetailStoresServiceTests {
+// MARK: - func repeatLastSearch()
+final class RepeatLastSearchTests: RetailStoresServiceTests {
     
 }
 
+// MARK: - func getStoreDetails(storeId:postcode:)
+final class GetStoreDetailsTests: RetailStoresServiceTests {
+    
+}
+
+// MARK: - func getStoreDeliveryTimeSlots(slots:storeId:startDate:endDate:location:)
+final class GetStoreDeliveryTimeSlotsTests: RetailStoresServiceTests {
+    
+}
+
+// MARK: - func getStoreCollectionTimeSlots(slots:storeId:startDate:endDate:)
+final class GetStoreCollectionTimeSlotsTests: RetailStoresServiceTests {
+    
+}
 
 extension RetailStoresSearch: PrefixRemovable { }

fabdaae9012bd58dd3d2421a5c37ce7652411c73
Henrik Gustavii <henrik.gustavii@snappyshopper.co.uk>
2022-06-15 12:14:36 +0100

    [SIB-262] Fixed flaky tests and code. Changed app icon to grey scale in order to differentiate from v1.

--- SnappyV2Tests/System/SnappyV2AppViewModelTests.swift
+++ SnappyV2Tests/System/SnappyV2AppViewModelTests.swift
@@ -21,20 +21,50 @@ class SnappyV2AppViewModelTests: XCTestCase {
     func test_givenInit_whenEnvironmentAppStateShowInitialViewSetToFalse_thenShowInitialViewIsFalse() {
         let sut = makeSUT()
         
-        sut.container.appState.value.routing.showInitialView = false
-        
         let expectation = expectation(description: "showInitialView")
         var cancellables = Set<AnyCancellable>()
         
         sut.$showInitialView
+            .first()
+            .receive(on: RunLoop.main)
+            .sink { _ in
+                sut.container.appState.value.routing.showInitialView = false
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+        
+        wait(for: [expectation], timeout: 2)
+        
+        XCTAssertEqual(sut.showInitialView, false)
+    }
+    
+    func test_givenInit_whenEnvironmentAppStateIsActiveSetToFalse_thenIsActiveIsFalse() {
+        let sut = makeSUT()
+        
+        let expectation = expectation(description: "showInitialView")
+        var cancellables = Set<AnyCancellable>()
+        
+        sut.$isActive
+            .first()
+            .receive(on: RunLoop.main)
             .sink { _ in
                 expectation.fulfill()
             }
             .store(in: &cancellables)
         
-        wait(for: [expectation], timeout: 5)
+        wait(for: [expectation], timeout: 2)
         
-        XCTAssertEqual(sut.showInitialView, false)
+        XCTAssertEqual(sut.isActive, false)
+    }
+    
+    func test_whenSetAppForegroundStatusIsSetToBackground_thenAppStateUpdatedToFalse() {
+        let sut = makeSUT()
+        
+        XCTAssertTrue(sut.container.appState.value.system.isInForeground)
+        
+        sut.setAppForegroundStatus(phase: .background)
+        
+        XCTAssertFalse(sut.container.appState.value.system.isInForeground)
     }
 
     func makeSUT() -> SnappyV2AppViewModel {

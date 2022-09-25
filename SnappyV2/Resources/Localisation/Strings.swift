86c180349b4a7168799efd1b15be84dd4edc34b0
Kevin Palser <kevin.palser@snappyshopper.co.uk>
2022-09-25 08:21:48 +0100

    pr feedback for localised text

--- SnappyV2/Resources/Localisation/Strings.swift
+++ SnappyV2/Resources/Localisation/Strings.swift
@@ -278,8 +278,6 @@ public enum Strings {
             case alertApply = "basketview.coupon.alertapply"
             case alertApplyShort = "basketview.coupon.alertapply.short"
             case alertRemove = "basketview.coupon.alertremove"
-            // case couponErrorTitle = "basketview.coupon.title"
-            // case couponErrorSubtitle = "basketview.coupon.subtitle"
         }
         
         public enum ListEntry: String, IterableSnappyString {
